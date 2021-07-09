2c83128ad42fd17067265ff55b667ef95027a4f3
Pragati Shah <pragati.shah87@gmail.com>
2021-07-09 08:56:53 +0530

    Changes for web hosting

--- library/settings.py
+++ library/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7k04u(&7%%7j@fp$&(lli_4j@dv)s^-wqc4q@shk&thz3=44be'
+SECRET_KEY = 'django-insecure-7kweqru(&7%%7j@fp$&(lli_4j@dv)s^-wqc4q@shk&thz3=44be'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['pragatishah.pythonanywhere.com',]
 
 
 # Application definition
